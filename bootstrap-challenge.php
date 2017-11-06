616309369680191bcb1610728bff0a2a9c7beee1
Jermain Jennings <jermainejennings05@gmail.com>
2017-11-06 08:17:48 -0700

    Start of Boot Strap. Boot Strap links added

--- bootstrap-challenge.php
+++ bootstrap-challenge.php
@@ -10,7 +10,17 @@
 
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>@media (min-width: 576px) { ... }
 		<h1>I DON'T KNOW WHAT IM DOING!</h1>
-	</body>
+		<section class="py-5 bg">
+			<div class="container">
+				</div>
+				<div class="row">
+					</div>
+					<div class="col">
+						</div>
+						<div class="jumbotron text-center">
+							</div>
+							<h1 class="display-2">I DON'T KNOW WHAT IM DOING!</h1>
+					</section>
 </html>
